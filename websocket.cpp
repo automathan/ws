e03f079de307c70e732fe74e78878846cd63f837
Jontahan <jona.jorg@gmail.com>
2017-04-30 23:24:11 +0200

    multithreading, buggy but works

--- websocket.cpp
+++ websocket.cpp
@@ -14,11 +14,14 @@
 #include <openssl/sha.h>
 #include "include/base64.h"
 #include <bitset>
+#include <thread>
+
 int main(int argc, char *argv[])
 {
     int listenfd = 0, connfd = 0;
     struct sockaddr_in serv_addr; 
-
+    struct sockaddr caddr;
+    
     time_t ticks; 
 
     listenfd = socket(AF_INET, SOCK_STREAM, 0);
@@ -34,7 +37,9 @@ int main(int argc, char *argv[])
     while(1)
     {
 	connfd = accept(listenfd, (struct sockaddr*)NULL, NULL);
-
+	socklen_t ll = 14;
+	getpeername(connfd, &caddr, &ll);
+	std::string address(caddr.sa_data);
 	std::cout << "connfd = " << connfd << std::endl;
 
 	char buffer[1400];
@@ -67,37 +72,43 @@ int main(int argc, char *argv[])
 	}else{
 	    std::cout << "<other>" << std::endl;
 	    reply =
-		"HTTP/1.1 200 OK\n"
+		"HTTP/1.1 200 OK\r\n"
 		"\n<script>var ws = new WebSocket('ws://192.168.10.117:8080');\n"
 		"ws.addEventListener('open',function(event){"
 		"\n\tconsole.log('open!!');\n\tws.send('yo bro!');\n});\n"
 		"var sendmessage = function(){console.log('click');ws.send('test123567890');};"
 		"ws.onmessage = function(event){console.log(event.data);};</script>"
-		"<input type=\"text\"><button onclick=\"sendmessage()\">send</button>";
+		"<input type=\"text\"><button onclick=\"sendmessage()\">send</button>\r\n";
 	}
 	send(connfd, reply.c_str(), reply.size(), 0);
+	std::cout << "sent " << (ws ? " socket accept" : "webpage") << " to " << connfd << " (" << address<<")" << std::endl;
 	if(ws){
-	    while(true){
-		char wsbuff[1400];
-		int abc = read(connfd, wsbuff, 1400);
-		if(abc > 0){
-		    std::cout << "received " << abc << " bytes:" << std::endl;
-		    char mask[4] = {wsbuff[2], wsbuff[3], wsbuff[4], wsbuff[5]};
-		    for(int i = 6; i < abc; ++i)
-			std::cout << char(wsbuff[i] ^ mask[(i - 2) % 4]);
-		    std::cout << std::endl;
-		    char* response = (char*)malloc(sizeof(char) * (abc - 4));
-		    response[0] = 0x81;
-		    response[1] = char(abc - 6);
-		    for(int i = 6; i < abc; ++i)
-			response[i - 4] = wsbuff[i] ^ mask[(i - 2) % 4];
-	     
-		    send(connfd, response, abc - 4, 0);
-		}
-		usleep(10);
-	    }
+	    std::cout << "starting new thread for " << connfd << " (" << address << ")"<< std::endl;
+	    std::thread thr([&connfd]() {
+		    int cpfd = connfd;
+		    while(true){
+			char wsbuff[1400];
+			int abc = read(cpfd, wsbuff, 1400);
+			if(abc > 0){
+			    std::cout << "received " << abc << " bytes:" << std::endl;
+			    char mask[4] = {wsbuff[2], wsbuff[3], wsbuff[4], wsbuff[5]};
+			    for(int i = 6; i < abc; ++i)
+				std::cout << char(wsbuff[i] ^ mask[(i - 2) % 4]);
+			    std::cout << std::endl;
+			    char* response = (char*)malloc(sizeof(char) * (abc - 4));
+			    response[0] = 0x81;
+			    response[1] = char(abc - 6);
+			    for(int i = 6; i < abc; ++i)
+				response[i - 4] = wsbuff[i] ^ mask[(i - 2) % 4];
+			    send(cpfd, response, abc - 4, 0);
+			}
+			usleep(10);
+		    }
+		});
+	    thr.detach();
+	    ws = false;
 	}
-	close(connfd);
-        sleep(1);
+	//close(connfd);
+        usleep(10);
      }
 }
